3242f1de8d5ff87fbaa3eb060062d8937cd91635
tobyfernandez <tobiasffernandez@gmail.com>
2024-10-26 10:58:17 -0600

    Finished lab 7

---
+++ lab7test.py
@@ -0,0 +1,5 @@
+from lab7 import caesar_cipher, create_table, view_table, zipper_merge
+
+assert caesar_cipher('Hello!') == 'Uryyb!'
+assert zipper_merge([1, 3, 5, 7, 9], [2, 4, 6, 8, 10]) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
